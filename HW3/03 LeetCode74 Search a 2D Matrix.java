11c0d8cfc5bff05f9f57363774e7523dcfc40ab2
001045854_Ming <wang.ming1@husky.neu.edu>
2020-02-21 11:01:51 -0800

    Update and rename  03 LeetCode74 Search a 2D Matrix to 03 LeetCode74 Search a 2D Matrix.java

--- HW3/03 LeetCode74 Search a 2D Matrix.java
+++ HW3/03 LeetCode74 Search a 2D Matrix.java
@@ -1,8 +1,11 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+    //check validity
         if(matrix == null || matrix.length <=0 || matrix[0].length <= 0){
             return false;
         }
+        //binary search, consider this 2D array as 1D array
+        //only thing we need to do is convert mid into row and column,
         for(int left = 0, n = matrix.length * matrix[0].length, right = n - 1; left <= right; ){
             int mid = (left + right)/2;
             int midR = mid/matrix[0].length;
